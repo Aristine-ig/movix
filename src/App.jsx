4a932d3375bd2b97e3517b58bda0fc7f49f95583
Aristine-ig <aristine277@gmail.com>
2023-09-10 14:44:43 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
-import SearchResult from "./pages/searchResult/searchResult";
 import Explore from "./pages/explore/Explore";
 import PageNotFound from "./pages/404/PageNotFound";
+import SearchResult from "./pages/searchResult/SearchResult";
 
 
 
